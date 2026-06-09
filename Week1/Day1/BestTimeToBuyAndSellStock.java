d8002937b2632127345b4e234637f6dc698fc8e7
Ayush kumar <ayushkumarsinha4542@gmail.com>
2026-06-09 12:53:59 +0530

    Week1 Day1 - Best Time to Buy and Sell Stock (Optimal)
    Refactored the maxProfit method to use an optimal O(n) approach for calculating maximum profit from stock prices.

--- Week1/Day1/BestTimeToBuyAndSellStock.java
+++ Week1/Day1/BestTimeToBuyAndSellStock.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int maxProfit(int[] prices) {
         int maxProfit=0;
         for(int i=0;i<prices.length;i++){
@@ -11,6 +11,26 @@ class Solution {
             }
         }
         return maxProfit;
+        
+    }
+}
+*/
+// optimal approach 0(n)
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min= prices[0];
+        int max=0; // maximum profit
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]<min){
+                min= prices[i];
+            }
+            int profit = prices[i]-min;
+            if(profit>max){
+                max=profit;
+            }
+
+        }
+        return max;
         
     }
 }
