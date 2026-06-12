f9fa64c564426fa30dd50c8f83126cec6ba91685
Ayush kumar <ayushkumarsinha4542@gmail.com>
2026-06-12 12:26:05 +0530

    Week1 Day4 -  Matrix Diagonal Sum
    Refactored diagonalSum method for optimal performance by reducing nested loops and handling odd-length matrices.

--- Week1/Day4/MatrixDiagonalSum.java
+++ Week1/Day4/MatrixDiagonalSum.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int diagonalSum(int[][] mat) {
         int sum=0;
         for(int i =0;i<mat.length;i++){
@@ -19,6 +19,22 @@ class Solution {
             }
         }
 
+        return sum;
+        
+    }
+}
+*/
+// optimal solution 
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int sum=0;
+        for(int i=0;i<mat.length;i++){
+            sum+=mat[i][i];
+            sum+=mat[i][mat[0].length-1-i];
+        }
+        if(mat.length%2!=0){
+            sum-=mat[mat.length/2][mat.length/2];
+        }
         return sum;
         
     }
