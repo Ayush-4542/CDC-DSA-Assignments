c6e08b6cf236de2c8f32cffbd1f924331d0869d9
Ayush kumar <ayushkumarsinha4542@gmail.com>
2026-06-12 15:31:15 +0530

    Week1 Day5 - Valid Palindrome

--- Week1/Day5/ValidPalindrome.java
+++ Week1/Day5/ValidPalindrome.java
@@ -1,21 +1,19 @@
 class Solution {
     public boolean isPalindrome(String s) {
         String str= s.toLowerCase();
-        String finalStr= "";
-        boolean isPalindrome = true;
+        StringBuilder finalStr= new StringBuilder();
         for(int i=0;i<str.length();i++){
             if((Character.isLetterOrDigit(str.charAt(i)))){
-                finalStr+=str.charAt(i); 
+                finalStr.append(str.charAt(i));
 
             }
         }
         for(int i=0;i<finalStr.length()/2;i++){
             if(finalStr.charAt(i)!= finalStr.charAt(finalStr.length()-1-i)){
-                isPalindrome = false;
-                break;
+                return false;
             }
         }
 
-        return isPalindrome;
+        return true;
     }
 }
