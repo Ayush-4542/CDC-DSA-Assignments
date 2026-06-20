7c9ddc7acc9aae82ad552c3589489a9ef300b654
Ayush kumar <ayushkumarsinha4542@gmail.com>
2026-06-20 12:51:12 +0530

    Week2 Day3 - Implement strStr()
    Implement the strStr function to find the index of the first occurrence of a substring.

---
+++ Week2/Day3/ImplementStrStr.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        StringBuilder sb = new StringBuilder();
+
+        if(haystack.length()>= needle.length()){
+            for(int i=0;i<needle.length();i++){
+                sb.append(haystack.charAt(i));
+            }
+            if(sb.toString().compareTo(needle)==0){
+                return 0;
+            }
+            for(int i=needle.length();i<haystack.length();i++){
+                sb.append(haystack.charAt(i));
+                sb.delete(0,1);
+                if(sb.toString().equals(needle)){
+                    return i-needle.length()+1;
+                }
+
+
+            }
+
+        }
+        return -1;
+        
+    }
+}
